439119f730d24002d85b0e9ad70f2129e41fa395
Frank Luongo <frankluongo92@gmail.com>
2019-01-05 13:20:09 -0500

    3: Android Setup & Emulation

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+// npm run ios and npm run android
+
 export default class App extends React.Component {
   render() {
     return (
