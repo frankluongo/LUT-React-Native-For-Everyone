f2646f5fa7002753813d7b83ddb044a6aa59fc89
Frank Luongo <frankluongo92@gmail.com>
2019-01-10 07:16:56 -0500

    10: Changing Tip Values With Buttons and Custom Tips

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, TextInput, Text, View } from 'react-native';
+import { StyleSheet, TextInput, Text, View, Button } from 'react-native';
 
 // npm run ios and npm run android
 // facebook.github.io for react native documentation
@@ -7,7 +7,8 @@ import { StyleSheet, TextInput, Text, View } from 'react-native';
 export default class App extends React.Component {
 
   state = {
-    inputValue: ""
+    inputValue: "",
+    tipAmount: 0.2
   }
 
   handleChange (text) {
@@ -19,18 +20,49 @@ export default class App extends React.Component {
   displayTipValue (number) {
     let tip = 0.00;
     if (number) {
-      tip = parseFloat(number) * 0.2;
+      tip = parseFloat(number) * this.state.tipAmount;
       tip = this.roundTip(tip)
     }
     return tip;
   }
 
+
+
   roundTip (tip) {
     return Math.round(tip * 100 / 100).toFixed(2);
   }
 
+  updateTipAmount (newAmount) {
+    this.setState({
+      tipAmount: newAmount
+    });
+  }
+
+  updateCustomTipAmount (customTip) {
+    if (customTip) {
+      customTip = parseFloat(customTip);
+      customTip = this.convertPercent(customTip);
+      this.setState({
+        tipAmount: customTip
+      });
+    } else {
+      this.setState({
+        tipAmount: 0
+      });
+    }
+  }
+
+  convertPercent (number) {
+    return number / 100;
+  }
+
+  convertTipAmountToPercent (tipAmount) {
+    let tipPercent = (tipAmount * 100).toString();
+    return tipPercent;
+  }
+
   render() {
-    let { inputValue } = this.state;
+    let { inputValue, tipAmount } = this.state;
     return (
       <View style={styles.container}>
         <Text>
@@ -43,6 +75,18 @@ export default class App extends React.Component {
           keyboardType="numeric"
           onChangeText={this.handleChange.bind(this)}
         />
+        <View style={styles.buttonGroup}>
+          <Button title="10%" onPress={this.updateTipAmount.bind(this, 0.1)} />
+          <Button title="20%" onPress={this.updateTipAmount.bind(this, 0.2)} />
+          <Button title="25%" onPress={this.updateTipAmount.bind(this, 0.25)} />
+          <TextInput
+            value={(tipAmount * 100).toString()}
+            placeholder="20%"
+            style={styles.customTip}
+            keyboardType="numeric"
+            onChangeText={this.updateCustomTipAmount.bind(this)}
+          />
+        </View>
       </View>
     );
   }
@@ -56,11 +100,21 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     padding: 20
   },
+  customTip: {
+    height: 40,
+    width: '60%',
+    borderColor: '#333',
+    borderWidth: 1,
+    padding: 5
+  },
   heading: {
     fontSize: 32
   },
   body: {
     fontSize: 24
+  },
+  buttonGroup: {
+    flexDirection: 'row'
   },
   input: {
     height: 40,
